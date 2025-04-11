c5c51753003972982d693ed794f4c6a8a3ba7b47
aleksandar.benefiti <aleksandar@benefiti.rs>
2025-04-11 11:35:28 +0200

    chore:fix code snippets break word on small screens and fix width for small screen elements by adding max-width

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -4,18 +4,18 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
 interface LayoutProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
-  return (
-    <main className="relative flex gap-4 p-4">
-      <SidebarProvider className="w-max hidden md:block">
-        <AppSidebar className="absolute top-0 left-0 " />
-        <SidebarTrigger className="md:hidden" />
-      </SidebarProvider>
+	return (
+		<main className="relative flex gap-4 p-4">
+			<SidebarProvider className="w-max hidden md:block">
+				<AppSidebar className="absolute top-0 left-0 " />
+				<SidebarTrigger className="md:hidden" />
+			</SidebarProvider>
 
-      <div className="flex-1 px-4 py-4">{children}</div>
-    </main>
-  );
+			<div className="flex-1 px-4 py-4 max-w-full">{children}</div>
+		</main>
+	);
 }
