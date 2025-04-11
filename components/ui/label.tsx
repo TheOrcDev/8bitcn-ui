cdd73fd1dddf38e49c49369765380ea61fe6878e
bdhamithkumara <damithkumararoxx55@gmail.com>
2025-04-12 00:02:55 +0530

    fix : fix the issue  #58

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -4,20 +4,24 @@ import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 
 import { cn } from "@/lib/utils"
+import { Checkbox } from "@/components/ui/8bit/checkbox";
 
 function Label({
   className,
   ...props
 }: React.ComponentProps<typeof LabelPrimitive.Root>) {
   return (
-    <LabelPrimitive.Root
-      data-slot="label"
-      className={cn(
-        "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
-        className
-      )}
-      {...props}
-    />
+    <div className={cn("relative flex items-center justify-center space-x-4", className)}>
+      <Checkbox />
+      <LabelPrimitive.Root
+        data-slot="label"
+        className={cn(
+          "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
+          className
+        )}
+        {...props}
+      />
+    </div>
   )
 }
 
