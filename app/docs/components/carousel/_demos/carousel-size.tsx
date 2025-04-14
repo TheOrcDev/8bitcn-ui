6e60847037899e7b84ee166f2a9c4076141dbd44
Riley1101 <arkardev00@gmail.com>
2025-04-14 10:30:59 -0400

    added registry and carousel demos

--- app/docs/components/carousel/_demos/carousel-size.tsx
+++ app/docs/components/carousel/_demos/carousel-size.tsx
@@ -7,20 +7,65 @@ import {
   CarouselPrevious,
 } from "@/components/ui/8bit/carousel"
 
-export function CarouselSize() {
+export function CarouselDemo() {
+  return (
+    <Carousel className="w-full max-w-xs">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
+
+export function CarouselSpacing() {
+  return (
+    <Carousel className="w-full max-w-sm">
+      <CarouselContent className="-ml-1">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-2xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
+
+export function OrientationVertical() {
   return (
     <Carousel
       opts={{
-        align: "start",
+        align: "center",
       }}
-      className="relative w-full max-w-xl"
+      orientation="vertical"
+      className="w-full max-w-xs"
     >
-      <CarouselContent>
+      <CarouselContent className="-mt-1 max-h-[200px]">
         {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index} className="md:basis-1/3">
-            <div className="p-2">
+          <CarouselItem key={index} className="pt-1 md:basis-1/2">
+            <div className="p-1">
               <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
+                <CardContent className="flex items-center justify-center p-6">
                   <span className="text-3xl font-semibold">{index + 1}</span>
                 </CardContent>
               </Card>
--- app/docs/components/carousel/page.tsx
+++ app/docs/components/carousel/page.tsx
@@ -1,9 +1,14 @@
-import CodeSnippet from "../code-snippet";
-import CopyCommandButton from "../copy-command-button";
-import InstallationCommands from "../installation-commands";
-import { CarouselSize } from "./_demos/carousel-size";
-import { OpenInV0Button } from "../open-in-v0-button";
-import { Separator } from "@/components/ui/separator";
+import { Separator } from "@/components/ui/separator"
+
+import CodeSnippet from "../code-snippet"
+import CopyCommandButton from "../copy-command-button"
+import InstallationCommands from "../installation-commands"
+import { OpenInV0Button } from "../open-in-v0-button"
+import {
+  CarouselDemo,
+  CarouselSpacing,
+  OrientationVertical,
+} from "./_demos/carousel-size"
 
 export default function CarouselPage() {
   return (
@@ -11,8 +16,8 @@ export default function CarouselPage() {
       <div className="flex flex-col md:flex-row items-center justify-between gap-2">
         <h1 className="text-3xl font-bold">Carousel</h1>
         <CopyCommandButton
-          copyCommand={`pnpm dlx shadcn@canary add ${process.env.NEXT_PUBLIC_BASE_URL}/r/8bit-button.json`}
-          command={"pnpm dlx shadcn@canary add 8bit-button"}
+          copyCommand={`pnpm dlx shadcn@canary add ${process.env.NEXT_PUBLIC_BASE_URL}/r/8bit-carousel.json`}
+          command={"pnpm dlx shadcn@canary add 8bit-carousel"}
         />
       </div>
 
@@ -31,7 +36,40 @@ export default function CarouselPage() {
           </div>
         </div>
         <div className="flex items-center justify-center min-h-[400px] relative">
-          <CarouselSize />
+          <CarouselDemo />
+        </div>
+      </div>
+
+      <p className="text-muted-foreground">
+        To set the spacing between the items, we use a <code>pl-[VALUE]</code>
+        utility on the <code>CarouselItem</code> and a negative
+        <code>-ml-[VALUE]</code> on the <code>CarouselContent</code>.
+      </p>
+
+      <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px]">
+        <div className="flex items-center justify-between">
+          <h2 className="text-sm text-muted-foreground sm:pl-3">
+            Spacing controlled carousel
+          </h2>
+        </div>
+        <div className="flex items-center justify-center min-h-[400px] md:min-h-[450px] relative">
+          <CarouselSpacing />
+        </div>
+      </div>
+
+      <p className="text-muted-foreground">
+        Use the <code>orientation</code> prop to set the orientation of the
+        carousel.
+      </p>
+
+      <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px]">
+        <div className="flex items-center justify-between">
+          <h2 className="text-sm text-muted-foreground sm:pl-3">
+            Orientation controlled carousel
+          </h2>
+        </div>
+        <div className="flex items-center justify-center min-h-[400px] md:min-h-[450px] relative">
+          <OrientationVertical />
         </div>
       </div>
 
@@ -40,16 +78,16 @@ export default function CarouselPage() {
       <Separator />
 
       <InstallationCommands
-        packageUrl={`${process.env.NEXT_PUBLIC_BASE_URL}/r/8bit-button.json`}
+        packageUrl={`${process.env.NEXT_PUBLIC_BASE_URL}/r/8bit-carousel.json`}
       />
 
       <h3 className="text-lg font-bold mt-10">Usage</h3>
 
       <Separator />
 
-      <CodeSnippet>{`import { Button } from "@/components/ui/8bit/button"`}</CodeSnippet>
+      <CodeSnippet>{`import { Button } from "@/components/ui/8bit/carousel"`}</CodeSnippet>
 
       <CodeSnippet>{`<Button variant="outline">Button</Button>`}</CodeSnippet>
     </div>
-  );
+  )
 }
