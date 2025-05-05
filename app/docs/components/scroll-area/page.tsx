8cecd173abf6f318ec670a249174d02ccf00c437
Riley1101 <arkardev00@gmail.com>
2025-05-04 21:45:15 -0400

    scroll area update

--- app/docs/components/scroll-area/page.tsx
+++ app/docs/components/scroll-area/page.tsx
@@ -1,17 +1,19 @@
 import { Metadata } from "next"
 
 import { buttonMetaData } from "@/lib/metadata"
-import { Button } from "@/components/ui/8bit/button"
 import { Separator } from "@/components/ui/separator"
 
 import CodeSnippet from "../code-snippet"
 import CopyCommandButton from "../copy-command-button"
 import InstallationCommands from "../installation-commands"
 import { OpenInV0Button } from "../open-in-v0-button"
-import { ScrollAreaDemo } from "./_demo/scrollarea-demo"
+import {
+  ScrollAreaDemo,
+  ScrollAreaHorizontalDemo,
+} from "./_demo/scrollarea-demo"
 
 export const metadata: Metadata = {
-  title: "8bit Button",
+  title: "8bit ScrollArea",
   description:
     "Displays a button or a component that looks like a 8-bit button.",
   openGraph: {
@@ -19,29 +21,29 @@ export const metadata: Metadata = {
   },
 }
 
-export default function ButtonPage() {
+export default function ScrollbarPage() {
   return (
     <div className="flex flex-col gap-4">
       <div className="flex flex-col md:flex-row items-center justify-between gap-2">
         <h1 className="text-3xl font-bold">Button</h1>
         <CopyCommandButton
-          copyCommand={`pnpm dlx shadcn@canary add ${process.env.NEXT_PUBLIC_BASE_URL}/r/8bit-button.json`}
-          command={"pnpm dlx shadcn@canary add 8bit-button"}
+          copyCommand={`pnpm dlx shadcn@canary add ${process.env.NEXT_PUBLIC_BASE_URL}/r/8bit-scroll-area.json`}
+          command={"pnpm dlx shadcn@canary add 8bit-scroll-area"}
         />
       </div>
 
       <p className="text-muted-foreground">
-        Displays a button or a component that looks like a 8-bit button.
+        Augments native scroll functionality for custom, cross-browser styling.
       </p>
 
       <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px]">
         <div className="flex items-center justify-between">
           <h2 className="text-sm text-muted-foreground sm:pl-3">
-            A simple 8-bit button component
+            A simple 8-bit scroll area component
           </h2>
 
           <div className="flex items-center gap-2">
-            <OpenInV0Button name="8bit-button" className="w-fit" />
+            <OpenInV0Button name="8bit-scroll-area" className="w-fit" />
           </div>
         </div>
         <div className="flex items-center justify-center min-h-[400px] relative">
@@ -54,16 +56,42 @@ export default function ButtonPage() {
       <Separator />
 
       <InstallationCommands
-        packageUrl={`${process.env.NEXT_PUBLIC_BASE_URL}/r/8bit-button.json`}
+        packageUrl={`${process.env.NEXT_PUBLIC_BASE_URL}/r/8bit-scroll-area.json`}
       />
+
+      <h3 className="text-lg font-bold mt-10">Horizontal Scrolling</h3>
+
+      <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px]">
+        <div className="flex items-center justify-between">
+          <h2 className="text-sm text-muted-foreground sm:pl-3">
+            A simple 8-bit scroll area component
+          </h2>
+
+          <div className="flex items-center gap-2">
+            <OpenInV0Button name="8bit-scroll-area" className="w-fit" />
+          </div>
+        </div>
+        <div className="flex items-center justify-center min-h-[400px] relative">
+          <ScrollAreaHorizontalDemo />
+        </div>
+      </div>
+
+      <Separator />
 
       <h3 className="text-lg font-bold mt-10">Usage</h3>
 
       <Separator />
 
-      <CodeSnippet>{`import { Button } from "@/components/ui/8bit/button"`}</CodeSnippet>
+      <CodeSnippet>{`import { ScrollArea } from "@/components/ui/scroll-area"`}</CodeSnippet>
 
-      <CodeSnippet>{`<Button variant="outline">Button</Button>`}</CodeSnippet>
+      <CodeSnippet>{`<ScrollArea className="h-[200px] w-[350px] rounded-md border p-4">
+  Jokester began sneaking into the castle in the middle of the night and leaving
+  jokes all over the place: under the king's pillow, in his soup, even in the
+  royal toilet. The king was furious, but he couldn't seem to stop Jokester. And
+  then, one day, the people of the kingdom discovered that the jokes left by
+  Jokester were so funny that they couldn't help but laugh. And once they
+  started laughing, they couldn't stop.
+</ScrollArea>`}</CodeSnippet>
     </div>
   )
 }
--- components/ui/8bit/scroll-area.tsx
+++ components/ui/8bit/scroll-area.tsx
@@ -1,12 +1,29 @@
 import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area"
 
 import { cn } from "@/lib/utils"
-import {
-  ScrollArea as ShadcnScrollArea,
-  ScrollBar as ShadcnScrollbar,
-} from "@/components/ui/scroll-area"
 
-const ScrollArea = ShadcnScrollArea
+function ScrollArea({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<typeof ScrollAreaPrimitive.Root>) {
+  return (
+    <ScrollAreaPrimitive.Root
+      data-slot="scroll-area"
+      className={cn("relative", className)}
+      {...props}
+    >
+      <ScrollAreaPrimitive.Viewport
+        data-slot="scroll-area-viewport"
+        className="focus-visible:ring-ring/50 size-full rounded-[inherit] transition-[color,box-shadow] outline-none focus-visible:ring-[3px] focus-visible:outline-1"
+      >
+        {children}
+      </ScrollAreaPrimitive.Viewport>
+      <ScrollBar />
+      <ScrollAreaPrimitive.Corner />
+    </ScrollAreaPrimitive.Root>
+  )
+}
 
 function ScrollBar({
   className,
@@ -14,10 +31,28 @@ function ScrollBar({
   ...props
 }: React.ComponentProps<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>) {
   return (
-    <ShadcnScrollbar
-      className={cn("bg-red-300", className)}
+    <ScrollAreaPrimitive.ScrollAreaScrollbar
+      data-slot="scroll-area-scrollbar"
+      orientation={orientation}
+      className={cn(
+        "flex touch-none p-px transition-colors select-none bg-foreground/30 dark:bg-ring/30 relative",
+        orientation === "vertical" &&
+          "h-full w-1.5 border-l border-l-transparent",
+        orientation === "horizontal" &&
+          "h-1.5 flex-col border-t border-t-transparent",
+        className
+      )}
       {...props}
-    ></ShadcnScrollbar>
+    >
+      <ScrollAreaPrimitive.ScrollAreaThumb
+        data-slot="scroll-area-thumb"
+        className={cn(
+          "relative dark:bg-ring rounded-none  flex-1 bg-foreground transition-none duration-75",
+          orientation === "vertical" && "scale-x-250 ",
+          orientation === "horizontal" && "scale-y-250 "
+        )}
+      />
+    </ScrollAreaPrimitive.ScrollAreaScrollbar>
   )
 }
 
