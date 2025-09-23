05976d59cf3446081bced50758f8ff0d820a0ef9
OrcDev <urosmiric88@gmail.com>
2025-09-23 17:58:12 +0200

    feat(blocks) add dashboard

---
+++ app/blocks/featured/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Featured",
+  description: "Featured",
+};
+
+export default function FeaturedPage() {
+  return <div>Featured</div>;
+}
