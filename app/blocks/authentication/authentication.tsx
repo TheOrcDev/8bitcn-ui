648364460d8e914e70478dbff6da5f6daa05e95b
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-09-26 18:58:39 +0300

    feat(blocks/authentication): add signup form with icons

--- app/blocks/authentication/authentication.tsx
+++ app/blocks/authentication/authentication.tsx
@@ -2,6 +2,7 @@ import { LoginForm } from "@/components/ui/8bit/blocks/login-form";
 import { LoginForm as LoginForm2 } from "@/components/ui/8bit/blocks/login-form-2";
 import { LoginForm as LoginFormWithImage } from "@/components/ui/8bit/blocks/login-form-with-image";
 import { SignupForm } from "@/components/ui/8bit/blocks/signup-form";
+import { SignupForm as SignupForm2 } from "@/components/ui/8bit/blocks/signup-form-2";
 
 import CopyCommandButton from "../../docs/components/copy-command-button";
 import { OpenInV0Button } from "../../docs/components/open-in-v0-button";
@@ -125,6 +126,25 @@ export default function AuthenticationBlocks() {
       <div className="flex items-center justify-center min-h-[400px] relative border rounded-md">
         <div className="w-full">
           <SignupPage />
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px]">
+        <div className="flex flex-col md:flex-row gap-2 items-center justify-between">
+          <h2 className="text-sm text-muted-foreground sm:pl-3">
+            A simple signup form with icons
+          </h2>
+
+          <div className="flex flex-col md:flex-row items-center gap-2">
+            <CopyCommandButton
+              command="pnpm dlx shadcn@latest add @8bitcn/signup-form-2"
+              copyCommand="pnpm dlx shadcn@latest add @8bitcn/signup-form-2"
+            />
+            <OpenInV0Button name="8bit-signup-form-2" className="w-fit" />
+          </div>
+        </div>
+        <div className="flex items-center justify-center min-h-[400px] relative">
+          <SignupForm2 />
         </div>
       </div>
     </>
