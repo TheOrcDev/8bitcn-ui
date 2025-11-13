9a27a745494fe0062933e0da6bf06f0f6a13ae78
OrcDev <urosmiric88@gmail.com>
2025-11-13 12:36:36 +0100

    feat: update Sponsors component to dynamically render sponsors and add Trigger.dev logo

--- components/sponsors.tsx
+++ components/sponsors.tsx
@@ -1,13 +1,26 @@
 import Link from "next/link";
 
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/8bit/avatar";
+import { cn } from "@/lib/utils";
+
+import { Avatar, AvatarImage } from "@/components/ui/8bit/avatar";
 
 import { Button } from "./ui/8bit/button";
 import { Separator } from "./ui/8bit/separator";
+
+const sponsors = [
+  {
+    name: "Shadcn Blocks",
+    url: "https://www.shadcnblocks.com/",
+    image: "/sponsors/shadcn-blocks.svg",
+    invert: true,
+  },
+  {
+    name: "Trigger.dev",
+    url: "https://www.trigger.dev/",
+    image: "/sponsors/trigger-dev.png",
+    invert: false,
+  },
+];
 
 export default function Sponsors() {
   return (
@@ -24,19 +37,22 @@ export default function Sponsors() {
 
       <h2 className="text-xl font-bold">LEGENDARY Sponsors</h2>
 
-      <Link href="https://www.shadcnblocks.com/" target="_blank">
-        <div className="flex flex-col gap-4 items-center">
-          <Avatar className="size-60" variant="default">
-            <AvatarImage
-              src="/sponsors/shadcn-blocks.svg"
-              alt="Shadcn Blocks"
-              className="dark:invert"
-            />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <h3 className="text-sm font-bold">Shadcn Blocks</h3>
-        </div>
-      </Link>
+      <div className="flex gap-10">
+        {sponsors.map((sponsor) => (
+          <Link href={sponsor.url} target="_blank" key={sponsor.name}>
+            <div className="flex flex-col gap-4 items-center">
+              <Avatar className="size-60" variant="default">
+                <AvatarImage
+                  src={sponsor.image}
+                  alt={sponsor.name}
+                  className={cn("p-5", sponsor.invert && "dark:invert")}
+                />
+              </Avatar>
+              <p className="text-sm font-bold">{sponsor.name}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
 
       <div className="flex flex-col gap-5 items-center">
         <p className="text-sm text-muted-foreground max-w-xl text-center">
