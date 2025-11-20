54a78de8cb9ded85a19a12cc5f3987203cdacc49
OrcDev <urosmiric88@gmail.com>
2025-11-20 13:45:42 +0100

    feat(app) add Wandry Analytics (#455)

---
+++ proxy.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { captureRegistryEvent } from "@wandry/analytics-sdk";
+
+export function proxy(request: NextRequest) {
+  captureRegistryEvent(request, process.env.WANDRY_ANALYTICS_TOKEN ?? "");
+  return NextResponse.next();
+}
