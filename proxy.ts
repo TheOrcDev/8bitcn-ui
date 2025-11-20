b6cc77fac553d58b9f7d9a4ef60b6537475c2808
OrcDev <urosmiric88@gmail.com>
2025-11-20 14:53:32 +0100

    fix(proxy): update analytics token environment variable to NEXT_PUBLIC_WANDRY_ANALYTICS_TOKEN

--- proxy.ts
+++ proxy.ts
@@ -3,6 +3,9 @@ import { NextRequest, NextResponse } from "next/server";
 import { captureRegistryEvent } from "@wandry/analytics-sdk";
 
 export function proxy(request: NextRequest) {
-  captureRegistryEvent(request, process.env.WANDRY_ANALYTICS_TOKEN ?? "");
+  captureRegistryEvent(
+    request,
+    process.env.NEXT_PUBLIC_WANDRY_ANALYTICS_TOKEN ?? ""
+  );
   return NextResponse.next();
 }
