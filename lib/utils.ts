ba84b28b6eba0346257e2f4b3f6be9c1104afc6a
Ritesh9876 <riteshmangdare@gmail.com>
2025-12-08 20:59:58 +0530

    feat(components): implement new friend list component block

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,3 +8,11 @@ export function cn(...inputs: ClassValue[]) {
 export function absoluteUrl(path: string) {
   return `${process.env.NEXT_PUBLIC_BASE_URL}${path}`;
 }
+
+export const truncate = (
+  str: string | null | undefined,
+  length: number
+): string | null => {
+  if (!str || str.length <= length) return str ?? null;
+  return `${str.slice(0, length - 3)}...`;
+};
