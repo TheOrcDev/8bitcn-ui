b6ab4433c96a56b90c0f082006bb657b9222b8ac
OrcDev <urosmiric88@gmail.com>
2026-03-18 12:35:30 +0100

    feat(mobile-nav): replace menu icon with SVG for improved accessibility and styling

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -20,7 +20,45 @@ export default function MobileNav() {
     <Drawer onOpenChange={setOpen} open={open}>
       <DrawerTrigger asChild>
         <Button size="icon" variant="ghost">
-          ☰
+          <svg
+            aria-label="align-left"
+            className="size-7"
+            fill="currentColor"
+            height="50"
+            stroke="currentColor"
+            strokeWidth="0.25"
+            viewBox="0 0 256 256"
+            width="50"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <rect height="14" rx="1" width="14" x="64" y="72" />
+            <rect height="14" rx="1" width="14" x="80" y="72" />
+            <rect height="14" rx="1" width="14" x="96" y="72" />
+            <rect height="14" rx="1" width="14" x="128" y="72" />
+            <rect height="14" rx="1" width="14" x="144" y="72" />
+            <rect height="14" rx="1" width="14" x="160" y="72" />
+            <rect height="14" rx="1" width="14" x="176" y="72" />
+            <rect height="14" rx="1" width="14" x="192" y="72" />
+            <rect height="14" rx="1" width="14" x="48" y="72" />
+            <rect height="14" rx="1" width="14" x="112" y="72" />
+            <rect height="14" rx="1" width="14" x="64" y="168" />
+            <rect height="14" rx="1" width="14" x="80" y="168" />
+            <rect height="14" rx="1" width="14" x="96" y="168" />
+            <rect height="14" rx="1" width="14" x="128" y="168" />
+            <rect height="14" rx="1" width="14" x="144" y="168" />
+            <rect height="14" rx="1" width="14" x="160" y="168" />
+            <rect height="14" rx="1" width="14" x="176" y="168" />
+            <rect height="14" rx="1" width="14" x="48" y="120" />
+            <rect height="14" rx="1" width="14" x="64" y="120" />
+            <rect height="14" rx="1" width="14" x="48" y="168" />
+            <rect height="14" rx="1" width="14" x="112" y="168" />
+            <rect height="14" rx="1" width="14" x="80" y="120" />
+            <rect height="14" rx="1" width="14" x="96" y="120" />
+            <rect height="14" rx="1" width="14" x="128" y="120" />
+            <rect height="14" rx="1" width="14" x="144" y="120" />
+            <rect height="14" rx="1" width="14" x="112" y="120" />
+          </svg>
         </Button>
       </DrawerTrigger>
       <DrawerContent className="h-3/4">
