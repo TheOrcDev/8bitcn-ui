3cabf327a87b7a1a690254647b97f176a60938f3
OrcDev <urosmiric88@gmail.com>
2026-03-23 21:04:04 +0100

    feat: use undead 404 block as site not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import NotFound1 from "@/components/ui/8bit/blocks/not-found1";
+import NotFound3 from "@/components/ui/8bit/blocks/not-found3";
 
 export const metadata: Metadata = {
   title: "404 | 8bitcn/ui",
 };
 
 export default function NotFound() {
-  return <NotFound1 />;
+  return <NotFound3 className="h-screen" />;
 }
